501be1c1ab1b973e33d6bc4885915657af35a388
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-09-18 11:32:41 +0200

    complete challenge

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,12 @@
 import { createServer } from "node:http";
+import { Chance } from "chance";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  const chance = new Chance();
+  const name = chance.name({ prefix: true });
+  const age = chance.age();
+  const profession = chance.profession({ rank: true });
+  response.end(
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+  );
+});
