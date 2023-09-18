139dadd1fd455bcd95129dd2c587b8cdff2da070
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-09-18 11:07:29 +0200

    complete challenge

--- backend-basics_routes/server.js
+++ backend-basics_routes/server.js
@@ -1,3 +1,15 @@
+import { create } from "node:domain";
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((response, request) => {
+  if (request.url === "/api/fish/1") {
+    response.statusCode = 200;
+    response.end("Shrimp");
+  } else if (request.url === "/api/fish/2") {
+    response.statusCode = 200;
+    response.end("Anemonefish");
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
+  }
+});
