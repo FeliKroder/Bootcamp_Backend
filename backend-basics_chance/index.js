d3a2fdd5b57b535d91372a8270f923c4b3c96fd6
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-09-18 11:36:46 +0200

    add forgotten : after localhost

--- backend-basics_chance/index.js
+++ backend-basics_chance/index.js
@@ -2,5 +2,5 @@ import { server } from "./server.js";
 
 const port = 8000;
 server.listen(port, () => {
-  console.log(`Server is running at http://localhost${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
