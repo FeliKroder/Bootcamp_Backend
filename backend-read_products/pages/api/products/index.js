ddb1cb4a288813a28a25262e337d0c38e734210e
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-09-21 11:45:17 +0200

    complete challenge til bonus

--- backend-read_products/pages/api/products/index.js
+++ backend-read_products/pages/api/products/index.js
@@ -1,5 +1,11 @@
-import { products } from "../../../lib/products";
+import Product from "@/db/models/Product.js";
+import dbConnect from "db/connect.js";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const products = await Product.find();
+    response.status(200).json(products);
+  }
 }
