d2a584db782489dfc4ef85cb42f170c145b00840
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-09-25 11:43:11 +0200

    send PUT request

--- backend-create_products/components/Product/index.js
+++ backend-create_products/components/Product/index.js
@@ -1,14 +1,36 @@
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
+import ProductForm from "../ProductForm";
 import Comments from "../Comments";
 import { StyledLink } from "../Link/Link.styled";
+import { useState } from "react";
 
 export default function Product() {
+  const [isEditMode, setisEditMode] = useState(false);
+
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch("/api/products/", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+    if (response.ok) {
+      mutate();
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -27,7 +49,15 @@ export default function Product() {
       </p>
       {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
       <StyledLink href="/">Back to all</StyledLink>
-      <StyledLink href="/">Delete</StyledLink>
+      <button
+        type="button"
+        onClick={() => {
+          setisEditMode(!isEditMode);
+        }}
+      >
+        Edit
+      </button>
+      {isEditMode && <ProductForm onSubmit={handleEditProduct} />}
     </ProductCard>
   );
 }
